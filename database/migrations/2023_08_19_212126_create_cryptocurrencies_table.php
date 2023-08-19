af89227ff8ca36b1df952c28cc971f7ba9700c6f
steeven-louk <steevenlouk.sl@gmail.com>
2023-08-19 23:55:13 +0200

    ajout des images des cryptos, et mise en place de la migration et du seeder de la table cryptoCurrency

---
+++ database/migrations/2023_08_19_212126_create_cryptocurrencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCryptocurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cryptocurrencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            // $table->string('devise', 3);
+            $table->string('logo', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cryptocurrencies');
+    }
+}
---
+++ database/seeders/CryptoCurrenciesSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+// use App\Models\cryptoCurrencies;
+
+use App\Models\cryptocurrencies;
+use Illuminate\Database\Seeder;
+
+class CryptoCurrenciesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currency =[
+            ["name" => "bitcoin", "logo"=>"bitcoin"],
+            ["name" => "cardano", "logo"=>"cardano"],
+            ["name" => "litecoin", "logo"=>"litecoin"],
+            ["name" => "dash", "logo"=>"dash"],
+            ["name" => "ethereum", "logo"=>"ethereum"],
+            ["name" => "iota", "logo"=>"iota"],
+            ["name" => "nem", "logo"=>"nem"],
+            ["name" => "ripple", "logo"=>"ripple"],
+            ["name" => "stellar", "logo"=>"stellar"],
+           
+        ];
+
+        foreach ($currency as $crypto) {
+           cryptocurrencies::create($crypto);
+        }
+    }
+}
